e9f63582e9c1090a163a8d9374d0659207091028
Yoko Hyakuna <yoko@hashicorp.com>
2020-08-19 17:22:50 -0700

    Update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,9 +15,8 @@ data "vault_policy_document" "autounseal" {
 # Create 'autounseal' policy
 resource "vault_policy" "autounseal" {
     name   = "autounseal"
-    policy = "${data.vault_policy_document.autounseal.hcl}"
+    policy = data.vault_policy_document.autounseal.hcl
 }
-
 
 # Enable Transit secrets engine
 resource "vault_mount" "transit" {
@@ -29,4 +28,5 @@ resource "vault_transit_secret_backend_key" "key" {
   depends_on = [vault_mount.transit]
   backend = "transit"
   name    = "autounseal-key"
+  deletion_allowed = true
 }
